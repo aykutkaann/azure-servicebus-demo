d96930d6521a353ec3fb8afbbc1fb6620e6c8163
vboxuser <vboxuser@Ubuntu1.modem.turktelekom>
2026-03-08 00:50:46 +0000

    feat: terraform infrastructure and github action pipeline added

---
+++ README.md
@@ -0,0 +1,10 @@
+# Azure Service Bus + Container Apps Jobs
+
+ Event-driven serverless architecture.
+## Architecture
+- **Azure Service Bus** → Message queue
+- **Azure Container Apps Jobs** → Message handler
+- **KEDA** → Auto Scale
+- **Terraform** → IaC
+- **GitHub Actions** → CI/CD pipeline
+
--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_servicebus_queue" "queue" {
 ## ACR
 
 resource "azurerm_container_registry" "acr" {
-    name = var.acr.name
+    name = "acreventdrivendemo"
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     sku = "Basic"
